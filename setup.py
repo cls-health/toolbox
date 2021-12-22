2caed57fa6fc97c025f3fae71ec9adc3df3d9905
Ibrahim Albaba <ibjbaba@gmail.com>
2021-12-22 11:48:14 -0600

    0.0.2

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import setuptools
 
 setuptools.setup(
     name='toolbox',
-    version='0.0.1',
+    version='0.0.2',
     author='Ibrahim Albaba',
     author_email='ibrahim@uvita-digital.com',
     description='CLS Stream Toolbox',
-    long_description="This toolbox is the coolest thing to ever happen to you",
-    long_description_content_type="text/markdown",
+    # long_description="This toolbox is the coolest thing to ever happen to you",
+    # long_description_content_type="text/markdown",
     url='https://github.com/ibrahim-uvita/toolbox',
     project_urls = {
         "Bug Tracker": "https://github.com/ibrahim-uvita/toolbox/issues"
     },
     license='MIT',
-    py_modules=['functions'],
+    packages=setuptools.find_packages(),
     install_requires=['sqlalchemy'],
 )
