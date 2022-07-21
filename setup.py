70b204abf93a5d31fcdd8dd90d6743050be738ec
fresh4 <15371721+fresh4@users.noreply.github.com>
2022-07-21 13:40:12 -0500

    updated toolbox version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name="toolbox",
-    version="0.1.3",
+    version="0.1.4",
     author="Ibrahim Albaba",
     author_email="ibrahim@uvita-digital.com",
     description="CLS Stream Toolbox",
